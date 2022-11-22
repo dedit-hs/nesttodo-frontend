6cb639a2fa1cc08fa15fb5aafc171c94ccf74c06
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2022-11-23 00:03:17 +0700

    nesttodo-frontend first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-front';
+  showMenu: boolean = true;
+  username = '';
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit() {
+    this.apiService.jwtUserToken.subscribe(token => {
+      if (token) {
+        const decoded: any = jwtDecode(token);
+        this.username = decoded.username;
+      }
+
+      if (this.username) {
+        this.showMenu = false;
+      } else {
+        this.showMenu = true;
+      }
+    });
+  }
+
+  logout() {
+    this.username = '';
+    this.username = this.apiService.logout();
+  }
 }
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { ApiService } from '../services/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.apiService.jwtUserToken.pipe(
+      map((result) => !!result),
+      tap(result => {
+        if (!result) {
+          this.router.navigateByUrl('/login').then();
+          return result;
+        }
+        return result;
+      })
+    );
+  }
+  
+}
